ce732704f0403609339010c059df48e1c6a7175a
AndyNoUse <62490458+AndyNoUse@users.noreply.github.com>
2024-11-02 14:40:45 +0100

    HelloMaster

--- src/Excercise11Github/Helloworld.java
+++ src/Excercise11Github/Helloworld.java
@@ -4,5 +4,7 @@ public class Helloworld {
     public static void main(String[] args) {
         System.out.println("Hello Dog!");
         System.out.println("Hello cat!");
+
+        System.out.println("Hello world!!");
     }
 }
