cd88b8a526d0741e8910ef8d809b86d7a9d83c3b
AndyNoUse <62490458+AndyNoUse@users.noreply.github.com>
2024-11-02 14:45:30 +0100

    More code!!!

--- src/Excercise11Github/Helloworld.java
+++ src/Excercise11Github/Helloworld.java
@@ -4,7 +4,7 @@ public class Helloworld {
     public static void main(String[] args) {
         System.out.println("Hello Dogs!");
         System.out.println("Hello cats!");
-
+        System.out.println("Even more code");
         System.out.println("Hello world!! How are you!?");
     }
 }
